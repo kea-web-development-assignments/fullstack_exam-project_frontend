534024fcce5ec49765fe2f5d28191464d86da838
Haidar Almajedi <haidar@deranged.dk>
2025-02-22 13:04:54 +0100

    enables deploying on digital ocean droplet

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
-import adapter from 'svelte-adapter-static-digitalocean';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({out: 'build',	precompress: false, envPrefix: '', polyfill: true}),
 	}
 };
 
