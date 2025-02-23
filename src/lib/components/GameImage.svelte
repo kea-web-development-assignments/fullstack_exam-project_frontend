6c151cf2d94c821583ffa19d30ccec8446c3b373
Haidar Almajedi <haidar@deranged.dk>
2025-02-23 17:23:04 +0100

    set image to fallback if undefined

--- src/lib/components/GameImage.svelte
+++ src/lib/components/GameImage.svelte
@@ -1,5 +1,6 @@
 <script>
     import placeholderImage from '$lib/assets/images/placeholder.png';
+    import { onMount } from 'svelte';
 
     export let elementClass = 'h-60 w-full object-cover rounded-t-md';
     export let src = '';
@@ -8,6 +9,12 @@
     function changeImageSrcToFallback(e) {
         e.target.src = placeholderImage;
     }
+
+    onMount(() => {
+        if(!src) {
+            src = placeholderImage;
+        }
+    });
 </script>
 
 <img
